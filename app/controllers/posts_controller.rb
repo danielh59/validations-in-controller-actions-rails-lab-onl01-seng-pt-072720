4281f40e3441ac4c7f99d953e33598f134b6d14a
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 09:37:40 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-      if @post(post_params).valid?
+      if @post.valid?
          @post.update(post_params)
 
         redirect_to post_path(@post)
