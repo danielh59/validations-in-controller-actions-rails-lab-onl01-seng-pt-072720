389ae1330a1b8a6fa6d59876e4482ebe4edae760
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 09:41:10 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post == Post.new(post_params)
+    @post = Post.find_by(params[:id])
 
       if @post.valid?
          @post.update(post_params)
