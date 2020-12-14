7901af1a6ce81734df73948522fd74d7c43d2905
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-14 09:25:41 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,7 +12,7 @@ class AuthorsController < ApplicationController
   if @author.valid?
 
      @author.save
-     @author = Author.create!(author_params)
+     @author = Author.create(author_params)
 
     redirect_to author_path(@author)
   else
